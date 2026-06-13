ec57d7eb3d700c718775e3f0761eb1d62dfff379
Kristo Erte <kristo.erte@gmail.com>
2026-06-13 15:21:39 +0300

    Init project and created config files etc...

---
+++ src/main/java/ee/bitweb/wordcloud_core/WordcloudCoreApplication.java
@@ -0,0 +1,13 @@
+package ee.bitweb.wordcloud_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcloudCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcloudCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/bitweb/wordcloud_core/WordcloudCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bitweb.wordcloud_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcloudCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
